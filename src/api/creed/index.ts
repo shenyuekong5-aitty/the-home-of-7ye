190a3148cc1ce9029f373784ae2582041ccd8216
空神月 <15857769+sky-god-moon@user.noreply.gitee.com>
2026-04-14 17:13:30 +0800

    fix: creedapi to service

--- src/api/creed/index.ts
+++ src/api/creed/index.ts
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 import type { CreedResponse } from './type'
 
 const API = {
-  LIST: '/api/creed/list'
+  LIST: '/creed/list'
 } as const
 
 export const reqGetCreedList = () => request.get<CreedResponse>(API.LIST)
