55181f2f3d30ebd122c55eb04ebb442eb0c7ea27
空神月 <15857769+sky-god-moon@user.noreply.gitee.com>
2026-04-14 17:10:29 +0800

    fix: quotoapi to service

--- src/api/quote/index.ts
+++ src/api/quote/index.ts
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 import type { QuoteResponse } from './type'
 
 const API = {
-  LIST: '/api/quote/list'
+  LIST: '/quote/list'
 } as const
 
 export const reqGetQuoteList = () => request.get<QuoteResponse>(API.LIST)
