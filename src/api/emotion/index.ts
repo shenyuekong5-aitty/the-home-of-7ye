d796350748d9e4bf6c8c5e9f9abf3edc815b0b31
空神月 <15857769+sky-god-moon@user.noreply.gitee.com>
2026-04-14 17:07:15 +0800

    fix: emotionapi to service

--- src/api/emotion/index.ts
+++ src/api/emotion/index.ts
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 import type { EmotionResponse } from './type'
 
 const API = {
-  EMOTIONLIST: '/api/emotion/list'
+  EMOTIONLIST: '/emotion/list'
 } as const
 
 export const reqGetEmotionList = () => request.get<EmotionResponse>(API.EMOTIONLIST)
