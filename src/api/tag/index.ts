c6d6d9ba7044542e76021c12508606683ec01d6a
空神月 <15857769+sky-god-moon@user.noreply.gitee.com>
2026-04-14 17:15:43 +0800

    fix: tagapi to service

--- src/api/tag/index.ts
+++ src/api/tag/index.ts
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 import type { TagResponse } from './type'
 
 const API = {
-  LIST: '/api/tag/list'
+  LIST: '/tag/list'
 } as const
 
 export const reqGetTagList = () => request.get<TagResponse>(API.LIST)
